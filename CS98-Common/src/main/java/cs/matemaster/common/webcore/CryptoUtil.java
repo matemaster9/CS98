bd87d4207a6ba5cb6331f5a02b099f7b2296089a
matemaster <matemaster@163.com>
2022-07-10 09:40:02 +0800

    CS98-Encrypt #comment CryptoUtil密码工具

---
+++ CS98-Common/src/main/java/cs/matemaster/common/webcore/CryptoUtil.java
@@ -0,0 +1,13 @@
+package cs.matemaster.common.webcore;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author MateMaster
+ * @since 2022/7/10
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CryptoUtil {
+
+}
