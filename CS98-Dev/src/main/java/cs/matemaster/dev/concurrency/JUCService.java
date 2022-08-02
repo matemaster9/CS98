876a7e9b474109e74e2528a48fe6b3088037b073
matemaster <matemaster@163.com>
2022-08-02 18:21:33 +0800

    CS98-v5-concurrency #comment [feat]线程池原理

--- CS98-Dev/src/main/java/cs/matemaster/dev/concurrency/JUCService.java
+++ CS98-Dev/src/main/java/cs/matemaster/dev/concurrency/JUCService.java
@@ -2,6 +2,8 @@ package cs.matemaster.dev.concurrency;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import cs.matemaster.web.common.constant.BizConstant;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
 
 import java.util.concurrent.*;
 
@@ -18,7 +20,24 @@ public class JUCService {
             BizConstant.MAXIMUM_POOL_SIZE,
             TimeUnit.SECONDS,
             new ArrayBlockingQueue<>(1000),
-            new ThreadFactoryBuilder().setNameFormat("concurrentService-").build(),
+            new ThreadFactoryBuilder().setNameFormat("BizService-%d").build(),
             new ThreadPoolExecutor.AbortPolicy()
     );
+
+    @Test
+    public void printThreadPoolExecutorProperty() {
+        String capacity = Integer.toBinaryString((1 << 29) - 1);
+        String running = Integer.toBinaryString(-1 << 29);
+        String shutdown = Integer.toBinaryString(0 << 29);
+        String stop = Integer.toBinaryString(1 << 29);
+        String tidying = Integer.toBinaryString(2 << 29);
+        String terminate = Integer.toBinaryString(3 << 29);
+
+        System.out.println(StringUtils.leftPad(capacity, 32,"0"));
+        System.out.println(StringUtils.leftPad(running, 32,"0"));
+        System.out.println(StringUtils.leftPad(shutdown, 32,"0"));
+        System.out.println(StringUtils.leftPad(stop, 32,"0"));
+        System.out.println(StringUtils.leftPad(tidying, 32,"0"));
+        System.out.println(StringUtils.leftPad(terminate, 32,"0"));
+    }
 }
